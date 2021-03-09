801e5091404b229d117fa541900c6b22b0494e91
ceronhermilo <ceronhermilo@yahoo.com>
2021-03-08 22:53:38 -0600

    Requiring models data into the server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express'); //from documentation: express is function
 const app = express();//app is an object
 
+// Import models
+const administrators=require('./models/administrators.js');
+const business=require('./models/business.js');
+const changeRequests=require('./models/change-requests.js');
+const clients=require('./models/clients.js');
+const ownerAprovals=require('./models/owner-aprovals.js');
+const peers=require('./models/peers.js');
+const ratings=require('./models/ratings.js');
+
 app.get('/', (req, res) => {
     res.send('Hello world via express');
 });
