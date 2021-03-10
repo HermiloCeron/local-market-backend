e89b1cf44699759208a820847124e251c3c4d697
ceronhermilo <ceronhermilo@yahoo.com>
2021-03-10 09:00:59 -0600

    Removing some console.logs

--- controllers/clients.js
+++ controllers/clients.js
@@ -50,18 +50,14 @@ const renderProfile=(req,res)=>{
 const signupClient=(req,res)=>{
     Counter.findByPk(1)
     .then(counters=>{
-        //console.log(counters)
         counters.clients++;
-        //console.log(counters)
         Counter.update(counters.dataValues,{
             where:{id:1},
             returning:true
         })
         .then(updatedCounter=>{
-            console.log(updatedCounter[1][0].dataValues)
             let newClientData=req.body;
             newClientData.clientId=updatedCounter[1][0].dataValues.clients;
-            console.log(newClientData)
             Client.create(newClientData)
             .then(newClient=>{
                 res.redirect(`/clients/profile/${newClient.clientId}`);
