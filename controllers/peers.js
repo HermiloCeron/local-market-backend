5ea25cbe487a1c9a227d512c6792ebbb8ca2bc7f
ceronhermilo <ceronhermilo@yahoo.com>
2021-03-11 18:46:55 -0600

    Updating updatePeers controller 2

--- controllers/peers.js
+++ controllers/peers.js
@@ -15,57 +15,58 @@ const correctionConstant=1.0/densityFunction(0,0,sigmaSquareConstant);
 let peerCounter=1;
 
 const updatePeers=(req,res)=>{
+    Peer.destroy({
+        where:{},
+        truncate:true
+    })
     Client.findAll({
         raw:true
     })
     .then(clients=>{
-        Peer.findAll({
+        let peers=[];
+        Rating.findAll({
             raw:true
         })
-        .then(peers=>{
-            Rating.findAll({
-                raw:true
-            })
-            .then(ratings=>{
-                clients.map(client => {
-                    let clientRatings=ratings.filter(rating => rating.clientId==client.clientId);
-                    clients.map(otherClient=>{
-                        if(otherClient.clientId!==client.clientId && client.locationId===otherClient.locationId){
-                            let otherClientRatings=ratings.filter(rating => rating.clientId==otherClient.clientId);
-                            let compatibilityLength=Math.floor(clientRatings.length*minimumCompatibilityLenghtIndex);
-                            if(compatibilityLength>0 && otherClientRatings.length>compatibilityLength){
-                                if(otherClientRatings.length<=clientRatings.length){
-                                    compatibilityIndex=otherClientRatings.length;
+        .then(ratings=>{
+            clients.map(client => {
+                let clientRatings=ratings.filter(rating => rating.clientId==client.clientId);
+                clients.map(otherClient=>{
+                    if(otherClient.clientId!==client.clientId && client.locationId===otherClient.locationId){
+                        let otherClientRatings=ratings.filter(rating => rating.clientId==otherClient.clientId);
+                        let compatibilityLength=Math.floor(clientRatings.length*minimumCompatibilityLenghtIndex);
+                        if(compatibilityLength>0 && otherClientRatings.length>compatibilityLength){
+                            if(otherClientRatings.length<=clientRatings.length){
+                                compatibilityIndex=otherClientRatings.length;
+                            }
+                            else{
+                                compatibilityIndex=clientRatings.length;
+                            }
+                            clientRatings.map(clientRate =>{
+                                otherClientRate=otherClientRatings.filter(otherClientRate => otherClientRate.businessId==clientRate.businessId);
+                                if(otherClientRate.length>0){
+                                    compatibility=densityFunction(otherClientRate[0].rating,clientRate.rating,sigmaSquareConstant);
+                                    incompatibility=1-compatibility*correctionConstant;
+                                    compatibilityIndex-=incompatibility;
+                                }else{
+                                    compatibilityIndex-=1;
                                 }
-                                else{
-                                    compatibilityIndex=clientRatings.length;
+                            })
+                            compatibilityindexPerUnit=compatibilityIndex/clientRatings.length;
+                            console.log(client.clientId,otherClient.clientId,compatibilityIndex,compatibilityindexPerUnit)
+                            if(compatibilityindexPerUnit>=minimumCompatibilityPerUnit){
+                                let newPeer={
+                                    peerId: peerCounter,
+                                    clientId: client.clientId,
+                                    peerClientId: otherClient.clientId,
+                                    compatibilityIndex: compatibilityindexPerUnit
                                 }
-                                clientRatings.map(clientRate =>{
-                                    otherClientRate=otherClientRatings.filter(otherClientRate => otherClientRate.businessId==clientRate.businessId);
-                                    if(otherClientRate.length>0){
-                                        compatibility=densityFunction(otherClientRate[0].rating,clientRate.rating,sigmaSquareConstant);
-                                        incompatibility=1-compatibility*correctionConstant;
-                                        compatibilityIndex-=incompatibility;
-                                    }else{
-                                        compatibilityIndex-=1;
-                                    }
-                                })
-                                compatibilityindexPerUnit=compatibilityIndex/clientRatings.length;
-                                console.log(client.clientId,otherClient.clientId,compatibilityIndex,compatibilityindexPerUnit)
-                                if(compatibilityindexPerUnit>=minimumCompatibilityPerUnit){
-                                    let newPeer={
-                                        peerId: peerCounter,
-                                        clientId: client.clientId,
-                                        peerClientId: otherClient.clientId,
-                                        compatibilityIndex: compatibilityindexPerUnit
-                                    }
-                                    peers.push(newPeer);
-                                    console.log(peers)
-                                    peerCounter++;
-                                }
+                                Peer.create(newPeer)
+                                // peers.push(newPeer);
+                                // console.log(peers)
+                                peerCounter++;
                             }
                         }
-                    })
+                    }
                 })
             })
         })
